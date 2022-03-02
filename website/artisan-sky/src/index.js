1ebfa61f534dd8698e34f76b99f91062b7864d30
Ian <hoyin.ching@gmail.com>
2022-03-02 12:24:10 +0800

    Using React in VS Code.

--- website/artisan-sky/src/index.js
+++ website/artisan-sky/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// eslint-disable-next-line no-unused-vars
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const element = React.createElement('h1', {className: 'greeting'}, 'Hello!');
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    element,
+    document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
