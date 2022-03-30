ffd2c1e1952cd7755be6ad151e76be33fb17dcae
michaelptf <1155142944@link.cuhk.edu.hk>
2022-03-30 14:15:52 +0800

    update server.js

--- website/artisan-sky/back-end/server.js
+++ website/artisan-sky/back-end/server.js
@@ -4,5 +4,6 @@ http.createServer(function (req, res) {
   fs.readFile('homepage.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
+    res.end();
   });
 }).listen(80);
