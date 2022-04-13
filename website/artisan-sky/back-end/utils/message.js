2c7f2e51bc214782b808ec7b43fc7fa15ead203e
lamch0 <gordonlamch@gmail.com>
2022-04-13 15:36:03 +0800

    timezone

--- website/artisan-sky/back-end/utils/message.js
+++ website/artisan-sky/back-end/utils/message.js
@@ -1,10 +1,10 @@
-const moment = require('moment');
+const moment = require('moment-timezone');
 
 function formatMsg(username, text) {
     return {
         username,
         text,
-        time: moment().format('h:mm a')
+        time: moment().tz("Asia/Taipei").format('h:mm a')
     }
 }
 
